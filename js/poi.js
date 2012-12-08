09f7295f4c4181fd45634caf7242c307dbcd4285
Dickson Tse <dtse87@gmail.com>
2012-12-08 15:04:45 -0800

    Remove Disqus code

--- js/poi.js
+++ js/poi.js
@@ -91,17 +91,3 @@ $(document).on('pagebeforeshow', '#poi-page', function() {
 
   initialize();
 });
-
-$(document).on('pageshow', '#poi-page', function() {
-  /* * * CONFIGURATION VARIABLES: EDIT BEFORE PASTING INTO YOUR WEBPAGE * * */
-  var disqus_shortname = 'acpoi';
-  var disqus_developer = 1;
-  var disqus_identifier = "http://acpoi.com/poi.html#" + selectedPOI.id;
-
-  /* * * DON'T EDIT BELOW THIS LINE * * */
-  (function() {
-      var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-      dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
-      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
-  })();
-});
