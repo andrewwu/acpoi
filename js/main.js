bfa0cbb5df6b1b8656d911889ed68e7bbcf7b664
Andrew Wu <wujuichang@gmail.com>
2012-12-08 11:12:02 -0800

    Add basic layout

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 var poiByCity = {};
 var cities = [];
+var selectedCity = null;
+var selectedPOI = null;
 
 $(document).on('pageinit', '#index-page', function() {
   $.getJSON('data.json', function(data) {
@@ -16,5 +18,20 @@ $(document).on('pageinit', '#index-page', function() {
       }
       poiByCity[val[8]].push(poi);
     });
+
+    for (var i = 0; i < cities.length; i++) {
+      $('select#city').append("<option value='" + cities[i] + "'>" + citites[i] + "</option>")
+    }
+  });
+
+  $('button#find').click(function(e) {
+    e.preventDefault(); 
+
+    selectedCity = $('select#city').val();
+    if (selectedCity == "") {
+      alert("Please select a city.");
+    } else {
+      $.mobile.changePage('city.html');
+    }
   });
 });
