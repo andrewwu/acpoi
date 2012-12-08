e051c43fdda32d969417c13869a77f2346ee8720
Dickson Tse <dtse87@gmail.com>
2012-12-08 13:30:44 -0800

    Integrate Google Places API result to POI page

--- js/poi.js
+++ js/poi.js
@@ -6,12 +6,7 @@ $(document).on('pageinit', '#poi-page', function() {
   if (selectedPOI.name != null) {
     name = selectedPOI.name; 
   }
-  $('h1#poi-name').text(name);
-
-  if (selectedPOI.location != null) {
-    location = selectedPOI.location;
-  }
-  $('span#poi-location').text(location);
+  $('h1#poi-name').text(name);  
 
   if (selectedPOI.telephone != null) {
     telephone = selectedPOI.telephone;
@@ -22,4 +17,64 @@ $(document).on('pageinit', '#poi-page', function() {
     e.preventDefault();
     history.back();
   });
+});
+
+$(document).on('pagebeforeshow', '#poi-page', function() {
+  var map;
+  var service;
+  var infowindow;
+
+  function initialize() {
+    var locationLatLng = new google.maps.LatLng(37.691971,-122.089005);
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        mapTypeId: google.maps.MapTypeId.ROADMAP,
+        center: locationLatLng,
+        zoom: 15
+      });
+
+    var request = {
+      location: locationLatLng,
+      radius: '5000',
+      query: selectedPOI.name
+    };
+
+    service = new google.maps.places.PlacesService(map);
+    service.textSearch(request, callback);
+  }
+
+  function callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      if (results.length >= 1) {
+        var place = results[0];
+        if (place.name == selectedPOI.name) {
+          $('span#poi-location').text(place.formatted_address);
+          $('div#map').removeClass('hidden');
+
+          createMarker(results[0]);
+          google.maps.event.trigger(map, "resize");
+          map.panTo(place.geometry.location);
+        }
+        else {
+          if (selectedPOI.location != null) {
+            $('span#poi-location').text(selectedPOI.location);
+          }
+        }
+      }
+      else {
+        if (selectedPOI.location != null) {
+          $('span#poi-location').text(selectedPOI.location);
+        }
+      }
+    }
+  }
+
+  function createMarker(place) {
+    var marker = new google.maps.Marker({
+      map: map,
+      position: place.geometry.location
+    });
+  }
+
+  initialize();
 });
